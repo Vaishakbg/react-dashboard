41969e46a1e72885a8d4497de4930c681ff1e953
Vaishakbg <vaishakbg@gmail.com>
2022-05-22 11:52:46 +0530

    Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,7 +28,8 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
 );
 
 const Navbar = () => {
-  const { activeMenu, setActiveMenu } = useStateContext();
+  const { activeMenu, setActiveMenu, handleClick, isClicked, setIsClicked } =
+    useStateContext();
 
   return (
     <div className="flex justify-between p-2 md:ml-6 md:mr-6 relative">
@@ -38,6 +39,52 @@ const Navbar = () => {
         icon={<AiOutlineMenu />}
         customFunc={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)}
       />
+
+      <div className="flex">
+        <NavButton
+          title="Cart"
+          color="blue"
+          icon={<FiShoppingCart />}
+          customFunc={() => handleClick("cart")}
+        />
+        <NavButton
+          title="Chat"
+          color="blue"
+          dotColor="#03C9D7"
+          icon={<BsChatLeft />}
+          customFunc={() => handleClick("chat")}
+        />
+        <NavButton
+          title="Notification"
+          dotColor="rgb(254, 201, 15)"
+          customFunc={() => handleClick("notification")}
+          color="blue"
+          icon={<RiNotification3Line />}
+        />
+        <TooltipComponent content="Profile" position="BottomCenter">
+          <div
+            className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg"
+            onClick={() => handleClick("userProfile")}
+          >
+            <img
+              className="rounded-full w-8 h-8"
+              src={avatar}
+              alt="user-profile"
+            />
+            <p>
+              <span className="text-gray-400 text-14">Hi,</span>{" "}
+              <span className="text-gray-400 font-bold ml-1 text-14">
+                Michael
+              </span>
+            </p>
+            <MdKeyboardArrowDown className="text-gray-400 text-14" />
+          </div>
+        </TooltipComponent>
+        {isClicked.cart && <Cart />}
+        {isClicked.chat && <Chat />}
+        {isClicked.notification && <Notification />}
+        {isClicked.userProfile && <UserProfile />}
+      </div>
     </div>
   );
 };
